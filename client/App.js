b7089aac157d720cf3601a6545097b8b34a1a5d4
april <martinezapril56@yahoo.com>
2018-06-22 16:18:41 -0400

    stilling trying to get client

---
+++ client/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route } from 'react-router-dom';
+import Signin from './auth/Signin';
+import Users from './users/Users';
+
+
+class App extends Component {
+  render()
+  {
+    
+    return (
+      <div className="App">
+        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Authentication using JWTs</h1>
+        </header>
+
+        <Route path="/signin" component={Signin}></Route>
+        <Route path="/users" component={Jokes} />
+      </div>
+    );
+  }
+}
+
+export default App;
